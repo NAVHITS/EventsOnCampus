3a62e3f9937282b239ab5fdacaedd749e58b86ca
Naveen S <naveen2aug@gmail.com>
2018-12-04 13:10:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-   define('DB_SERVER', '77.72.0.90');
-   define('DB_USERNAME', 'cnxjimpu_user1');
-   define('DB_PASSWORD', '_3cF)g~A+aMo');
-   define('DB_DATABASE', 'cnxjimpu_b2l');
-   define('DB_PORT', '3306');
+   define('DB_SERVER', '<db server address>');
+   define('DB_USERNAME', '<db username>');
+   define('DB_PASSWORD', '<db password>');
+   define('DB_DATABASE', '<db name>');
+   define('DB_PORT', '<db port>');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,DB_PORT);
 	if(!$db){
 		die("ERROR: Could not connect. " . mysqli_connect_error());
